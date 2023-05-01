13e5101ddf9f645132f24c77645b07a6ac8760f7
tariqul-dev <tariqulislam.dev@gmail.com>
2023-05-01 14:43:04 +0600

    string pattern matching

---
+++ String/kmp.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <class T>
+void print_v(vector<T> &vect)
+{
+    for (auto element : vect)
+    {
+        cout << element << " ";
+    }
+
+    cout << endl;
+}
+
+vector<int> createLps(string pattern)
+{
+    vector<int> arr(pattern.size());
+
+    arr[0] = 0;
+    int index = 0, i = 1;
+
+    while (i < pattern.size())
+    {
+        if (pattern[index] == pattern[i])
+        {
+            arr[i] = index + 1;
+            index++;
+            i++;
+        }
+        else
+        {
+            if (index != 0)
+                index = arr[index - 1];
+            else
+            {
+                arr[i] = index;
+                i++;
+            }
+        }
+    }
+
+    return arr;
+}
+
+void kmp(string text, string pattern)
+{
+    vector<int> lps = createLps(pattern);
+
+    int i = 0, j = 0;
+
+    bool isFound = false;
+
+    while (i < text.size())
+    {
+        if (text[i] == pattern[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            if (j != 0)
+            {
+                j = lps[j - 1];
+            }
+            else
+                i++;
+        }
+
+        if (j == pattern.size())
+        {
+            isFound = true;
+            break;
+        }
+    }
+
+    if (isFound){
+        cout << "Found" << endl;
+    }else {
+        cout << "Not Found" << endl;
+    }
+}
+
+int main()
+{
+
+    string text = "ababaaab";
+    string pattern = "abca";
+
+    kmp(text, pattern);
+
+    return 0;
+}
