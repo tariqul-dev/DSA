7b73dc9388d6692c4a8630f300dc3acca8ffa1bb
tariqul.islam <tariqulislam.dev@gmail.com>
2023-08-27 18:12:23 +0600

    tree and linked list

--- Python/Tree/BST/bst_insertion_traversel.py
+++ Python/Tree/BST/bst_insertion_traversel.py
@@ -4,15 +4,15 @@ class Node:
         self.left = None
         self.right = None
 
+
 class Tree:
     def __init__(self):
         self.root = None
 
-
     def _insert(self, node, data):
         if not node:
             return Node(data)
-        
+
         if data < node.data:
             node.left = self._insert(node.left, data)
         else:
@@ -24,10 +24,9 @@ class Tree:
         if not self.root:
             self.root = Node(data)
             return
-        
+
         self.root = self._insert(self.root, data)
 
-    
     def _print_inorder(self, node):
         if node:
             self._print_inorder(node.left)
@@ -36,7 +35,6 @@ class Tree:
 
     def print_inorder(self):
         self._print_inorder(self.root)
-
 
     def _print_preorder(self, node):
         if node:
@@ -47,7 +45,6 @@ class Tree:
     def print_preorder(self):
         self._print_preorder(self.root)
 
-
     def _print_post_order(self, node):
         if node:
             self._print_post_order(node.left)
@@ -57,16 +54,36 @@ class Tree:
     def print_post_order(self):
         self._print_post_order(self.root)
 
+    def _lca(self, root, v1, v2):
+        if not root:
+            return root
+
+        if root.data == v1 or root.data == v2:
+            return root
+
+        left = self._lca(root.left, v1, v2)
+        right = self._lca(root.right, v1, v2)
+
+        if left is None:
+            return right
+        if right is None:
+            return left
+
+        return root
+
+    def lca(self, n1, n2):
+        return self._lca(self.root, n1, n2)
 
 
-tree = Tree()
+if __name__ == '__main__':
+    tree = Tree()
 
-tree.insert(5)
-tree.insert(7)
-tree.insert(2)
+    tree.insert(5)
+    tree.insert(7)
+    tree.insert(2)
 
-tree.print_inorder()
-print()
-tree.print_preorder()
-print()
-tree.print_post_order()
+    tree.print_inorder()
+    print()
+    tree.print_preorder()
+    print()
+    tree.print_post_order()
---
+++ Python/Tree/BST/lowest_common_ancestor.py
@@ -0,0 +1,38 @@
+from bst_insertion_traversel import Tree
+
+if __name__ == '__main__':
+    tree = Tree()
+    # tree.insert(8)
+    # tree.insert(4)
+    # tree.insert(2)
+    # tree.insert(1)
+    # tree.insert(3)
+    # tree.insert(6)
+    # tree.insert(5)
+    # tree.insert(7)
+    # tree.insert(10)
+    # tree.insert(14)
+    # tree.insert(15)
+    # tree.insert(9)
+    # tree.insert(12)
+    # tree.insert(11)
+    # tree.insert(13)
+
+    tree.insert(4)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(1)
+    tree.insert(7)
+    tree.insert(6)
+
+    # n = int(input())
+    # for i in range(n):
+    #     tree.insert(int(input().strip()))
+    #
+    tree.print_inorder()
+
+    ans = tree.lca(1, 3)
+    print()
+
+    print(ans.data)
+
