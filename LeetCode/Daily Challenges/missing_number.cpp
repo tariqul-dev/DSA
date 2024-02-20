aaf318518691a2c54d591437e71c1b3b3a248251
tariqul-dev <tariqulislam.dev@gmail.com>
2024-02-20 22:03:25 +0600

    day-34

---
+++ LeetCode/Daily Challenges/missing_number.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int missingNumber(vector<int> &nums)
+{
+    int result = nums.size();
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        result ^= i ^ nums[i];
+    }
+
+    return result;
+}
+
+int main()
+{
+    vector<int> nums = {3, 0, 1};
+
+    cout << missingNumber(nums) << endl;
+
+    return 0;
+}
