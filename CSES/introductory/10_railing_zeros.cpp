5f9b96458b00f1186813387dc3fd5bbf9cdab09d
tariqul-dev <tariqul.exabyting@bkash.com>
2025-12-18 14:54:25 +0600

    day-5

---
+++ CSES/introductory/10_railing_zeros.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+
+    long long zeros = 0;
+    for (long long i = 5; i<= n; i *= 5){
+        zeros += n/i;
+    }
+
+    cout << zeros << endl;
+
+    return 0;
+}
